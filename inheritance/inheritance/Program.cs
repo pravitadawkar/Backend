bdf7ef65afc91ab1c0cac471654a5fdaeda1455b
Pravita Dawkar <pravitadawkar@gmail.com>
2022-02-23 16:34:37 +0530

    c#_inheritance

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace inheritance
+
+{
+    class Person //parent class
+    {
+        public string FirstName = "pravita";  // Person field
+        public string LastName = "dawkar";  // Person field
+        public void run()             // Person method 
+        {
+            Console.WriteLine(" pravita is good in run");
+        }
+    }
+    class Devloper:Person //child class
+    { 
+        public string Department = "IT ";// Devloper field
+        public void coding()             // Person method 
+        {
+            Console.WriteLine(" she is a good in devlopment");
+        }
+
+    }
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+           //create a new obj -----single inheritance-----
+            Devloper person1 = new Devloper();
+            Console.WriteLine(" Fulname:" + person1.FirstName + " " + person1.LastName );
+            person1.run();
+            Console.WriteLine("she is from" + person1.Department);
+            person1.coding();
+            
+
+
+
+        }
+    }
+    
+}
+
---
+++ multilevel_inheritance/multilevel_inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace multilevel_inheritance
+{
+    class Grandfather
+    {
+        public string surname = "dawkar";
+        public void work()
+        {
+            Console.WriteLine("he is farmer");
+        }
+    }
+    class Father : Grandfather
+    {
+        public int age = 50;
+        public void activity() 
+        {
+            Console.WriteLine("he is good swimer");
+        }
+    }
+    class Son : Father
+    {
+        public string profetion = "engineer";
+        public int expirience = 5;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new obj s
+            Son s = new Son();
+            Console.WriteLine(" my surname is:"+ s.surname);
+            Console.WriteLine("age of my father:"+s.age);
+            Console.WriteLine("my father:");
+            s.activity();
+            Console.WriteLine("my grandfather:");
+            s.work();
+
+            Console.Read();
+        }
+    }
+}
